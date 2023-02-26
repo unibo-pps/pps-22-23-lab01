3582419dc79bb979279f8765d52d6591f4ed90a3
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2023-02-26 19:52:30 +0100

    type: fourth call returns 1

--- pps-lab01-tdd/src/lab01/tdd/CircularList.java
+++ pps-lab01-tdd/src/lab01/tdd/CircularList.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 /**
  * Represents a list of integers, with a built-in iterator that is bidirectional and circular.
  * Example: with a list like {1,2,3}, the first call of next() returns 1, the second call returns 2,
- * the third returns 3, the fourth returns 4, and so on. Call to previous() goes in the other way.
+ * the third returns 3, the fourth returns 1, and so on. Call to previous() goes in the other way.
  */
 public interface CircularList {
 
