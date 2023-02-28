432b8b9e528cfc3fa13a7b4319a33a2686eea97a
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2023-02-28 14:41:38 +0100

    refactor: better namings

--- pps-lab01-tdd/test/lab01/tdd/step2/CircularListTest.java
+++ pps-lab01-tdd/test/lab01/tdd/step2/CircularListTest.java
@@ -3,7 +3,6 @@ package lab01.tdd.step2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
@@ -33,13 +32,13 @@ class CircularListTest {
     @Test
     void testForwardIterator() {
         this.populateList();
-        testIterator(List.of(1, 2, 3, 1), this.circularList::forwardIterator);
+        testIteratorYieldings(List.of(1, 2, 3, 1), this.circularList::forwardIterator);
     }
 
     @Test
     void testBackwardIterator() {
         this.populateList();
-        testIterator(List.of(3, 2, 1, 3), this.circularList::backwardIterator);
+        testIteratorYieldings(List.of(3, 2, 1, 3), this.circularList::backwardIterator);
     }
 
     private void populateList() {
@@ -63,32 +62,32 @@ class CircularListTest {
     @Test
     void testForwardIteratorOnSingleElement() {
         this.circularList.add(2);
-        testIterator(Collections.nCopies(100, 2), this.circularList::forwardIterator);
+        testIteratorYieldings(Collections.nCopies(100, 2), this.circularList::forwardIterator);
     }
 
     @Test
     void testBackwardIteratorOnSingleElement() {
         this.circularList.add(2);
-        testIterator(Collections.nCopies(100, 2), this.circularList::backwardIterator);
+        testIteratorYieldings(Collections.nCopies(100, 2), this.circularList::backwardIterator);
     }
 
     @Test
     void testNewForwardIteratorAfterAdd() {
         this.populateList();
-        testIterator(List.of(1, 2, 3, 1), this.circularList::forwardIterator);
+        testIteratorYieldings(List.of(1, 2, 3, 1), this.circularList::forwardIterator);
         this.circularList.add(4);
-        testIterator(List.of(1, 2, 3, 4, 1), this.circularList::forwardIterator);
+        testIteratorYieldings(List.of(1, 2, 3, 4, 1), this.circularList::forwardIterator);
     }
 
     @Test
     void testNewBackwardIteratorAfterAdd() {
         this.populateList();
-        testIterator(List.of(3, 2, 1, 3), this.circularList::backwardIterator);
+        testIteratorYieldings(List.of(3, 2, 1, 3), this.circularList::backwardIterator);
         this.circularList.add(4);
-        testIterator(List.of(4, 3, 2, 1, 4), this.circularList::backwardIterator);
+        testIteratorYieldings(List.of(4, 3, 2, 1, 4), this.circularList::backwardIterator);
     }
 
-    private void testIterator(List<Integer> expectedYields, Supplier<Iterator<Integer>> iteratorSupplier) {
+    private void testIteratorYieldings(List<Integer> expectedYields, Supplier<Iterator<Integer>> iteratorSupplier) {
         Iterator<Integer> iterator = iteratorSupplier.get();
         assertTrue(iterator.hasNext());
         isYieldingCorrectly(expectedYields, iterator);
