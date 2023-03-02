5c721d1f0ead604fc9759bc43ed03ddd1e9bcfc2
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2023-03-02 22:18:34 +0100

    refactor: use constants in place of magic numbers

--- pps-lab01-intellij-basic-example/test/SimpleBankAccountTest.java
+++ pps-lab01-intellij-basic-example/test/SimpleBankAccountTest.java
@@ -7,6 +7,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SimpleBankAccountTest extends AbstractSimpleBankAccountTest {
 
+    private static final int WRONG_USER_ID = 2;
+    private static final int DEPOSIT_AMOUNT = 100;
+    private static final int WITHDRAWAL_AMOUNT = 70;
+
     @BeforeEach
     void beforeEach() {
         this.accountHolder = new AccountHolder("Kel", "Vin", 1);
@@ -15,28 +19,28 @@ class SimpleBankAccountTest extends AbstractSimpleBankAccountTest {
 
     @Test
     void testDeposit() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        assertEquals(100, bankAccount.getBalance());
+        bankAccount.deposit(accountHolder.getId(), DEPOSIT_AMOUNT);
+        assertEquals(DEPOSIT_AMOUNT, bankAccount.getBalance());
     }
 
     @Test
     void testWrongDeposit() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        bankAccount.deposit(2, 50);
-        assertEquals(100, bankAccount.getBalance());
+        bankAccount.deposit(accountHolder.getId(), DEPOSIT_AMOUNT);
+        bankAccount.deposit(WRONG_USER_ID, DEPOSIT_AMOUNT);
+        assertEquals(DEPOSIT_AMOUNT, bankAccount.getBalance());
     }
 
     @Test
     void testWithdraw() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        bankAccount.withdraw(accountHolder.getId(), 70);
-        assertEquals(30, bankAccount.getBalance());
+        bankAccount.deposit(accountHolder.getId(), DEPOSIT_AMOUNT);
+        bankAccount.withdraw(accountHolder.getId(), WITHDRAWAL_AMOUNT);
+        assertEquals(DEPOSIT_AMOUNT - WITHDRAWAL_AMOUNT, bankAccount.getBalance());
     }
 
     @Test
     void testWrongWithdraw() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        bankAccount.withdraw(2, 70);
-        assertEquals(100, bankAccount.getBalance());
+        bankAccount.deposit(accountHolder.getId(), DEPOSIT_AMOUNT);
+        bankAccount.withdraw(WRONG_USER_ID, WITHDRAWAL_AMOUNT);
+        assertEquals(DEPOSIT_AMOUNT, bankAccount.getBalance());
     }
 }
